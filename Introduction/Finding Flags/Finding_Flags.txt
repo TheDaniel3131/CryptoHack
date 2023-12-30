4078f4b374b45fdf445a17381af44d249625e765
zephrous <danielpoh3131@gmail.com>
2023-12-30 21:59:10 +0800

    Introduction.

---
+++ Introduction/Finding Flags/Finding_Flags.txt
@@ -0,0 +1,1 @@
+crypto{y0ur_f1rst_fl4g}
---
+++ Introduction/Network Attacks/network_attacks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from pwn import * # pip install pwntools
+import json
+
+HOST = "socket.cryptohack.org"
+PORT = 11112
+
+r = remote(HOST, PORT)
+
+
+def json_recv():
+    line = r.readline()
+    return json.loads(line.decode())
+
+def json_send(hsh):
+    request = json.dumps(hsh).encode()
+    r.sendline(request)
+
+
+print(r.readline())
+print(r.readline())
+print(r.readline())
+print(r.readline())
+
+request = {
+    "buy": "flag" # For your infroamtion, it was "buy" : clothes before. In order to buy flag we have to send flag JSON object.
+}
+json_send(request)
+
+response = json_recv()
+
+print(response)
