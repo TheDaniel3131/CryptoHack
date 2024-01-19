9bb191d98361f34d7bc5c6d8b6172159211a4ee2
zephrous <danielpoh3131@gmail.com>
2024-01-19 17:15:03 +0800

    added RSA Starter 3

---
+++ RSA/RSA Starter 3/solve.py
@@ -0,0 +1,13 @@
+# N = p * q
+
+# p = 857504083339712752489993810777
+# q = 1029224947942998075080348647219
+
+# N = p * q
+# print(N)
+
+
+# Find the toilent of N. Below are the formula
+
+toilent_of_N = (p - 1) * (q - 1)
+print(toilent_of_N)
