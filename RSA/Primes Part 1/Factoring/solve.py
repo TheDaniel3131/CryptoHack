42c8582dad1b68145fdbebda7cde3268999ea66c
zephrous <danielpoh3131@gmail.com>
2024-01-24 09:27:03 +0800

    RSA Primes Part 1

---
+++ RSA/Primes Part 1/Factoring/solve.py
@@ -0,0 +1,12 @@
+# http://factordb.com/
+# https://alpertron.com.ar/ECM.HTM
+# https://github.com/elliptic-shiho/primefac-fork
+
+from sympy import factorint
+
+print(factorint(510143758735509025530880200653196460532653147))
+
+# answer should be the smaller one according to the question
+
+# output: {25889363174021185185929: 1, 19704762736204164635843: 1}
+# 19704762736204164635843
