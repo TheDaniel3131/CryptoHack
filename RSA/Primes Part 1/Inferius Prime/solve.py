3795066e5de32dbc2efa3dc408be659eb1fca6db
zephrous <danielpoh3131@gmail.com>
2024-01-25 22:09:59 +0800

    Inferius Prime

--- RSA/Primes Part 1/Inferius Prime/solve.py
+++ RSA/Primes Part 1/Inferius Prime/solve.py
@@ -9,7 +9,7 @@ ct = 39207274348578481322317340648475596807303160111338236677373  ## this is the
 ## we have N given .. now we can factorise it to get p and q and from there we get phi = (p-1)*(q-1) ...
 ## and then we can use d = inverse(e,phi) to get d
 ## then a = pow(c,d,n)
-## then m = long_to_bytes(a)  and print(a)
+## then m = long_to_bytes(a) and print(a)
 
 ## from factor.db we got :
 p = 752708788837165590355094155871
