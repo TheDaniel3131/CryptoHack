cc05f2927d003da00cf616bf97bc34866be790d5
zephrous <danielpoh3131@gmail.com>
2024-02-03 16:41:04 +0800

    Structure Of AES

---
+++ Symmetric Ciphers/Structure of AES/solve.py
@@ -0,0 +1,22 @@
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array.  """
+    return [byte for row in matrix for byte in row]
+
+
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+print(bytes2matrix(matrix))
+print(matrix2bytes(matrix))
+
+# To obtain the resulting plaintext, you can convert the matrix to bytes to plaintext format
+result = ''.join(map(chr, matrix2bytes(matrix)))
+print(result)
