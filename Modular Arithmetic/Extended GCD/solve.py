fb61c4ed7e7f45e20e347019c758e6f1b1c41a8e
zephrous <danielpoh3131@gmail.com>
2024-02-18 23:43:34 +0800

    update

--- Modular Arithmetic/Extended GCD/solve.py
+++ Modular Arithmetic/Extended GCD/solve.py
@@ -1,14 +1,25 @@
-def extended_gcd(p,q):
-    if p == 0:
-        return (q, 0, 1)
+def extended_gcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
     else:
-        (gcd, u, v) = extended_gcd(q % p, p)
-        return (gcd, v - (q // p) * u, u)
+        gcd, x, y = extended_gcd(b % a, a)
+        return (gcd, y - (b // a) * x, x)
 
+# Given primes p and q
 p = 26513
 q = 32321
 
+# Calculate gcd(p, q), and the coefficients u and v
 gcd, u, v = extended_gcd(p, q)
-print("[+] GCD: {}".format(gcd))
-print("[+] u,v: {},{}".format(u,v))
-print("\n[*] FLAG: crypto{{{},{}}}".format(u,v))
+
+# Ensure that u is the lower number
+if u < v:
+    print(u)
+else:
+    print(v)
+
+
+# Enter whichever of u and v is the lower number as the flag.
+# Answer: -8404
+
+# crypto{10245, -8404}
